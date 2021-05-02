03c310ae385a729ad4f1bf710a16a70ef89393aa
riri3 <kamijooor@gmail.com>
2021-05-02 14:25:24 +0000

    [finish]5-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '9f5aa0d1d88aee4c9c6aaf368d338192a1de9de4ca7a378a2f7c6fffc39d630dcaef5bafd0b458fc2ba160f8772c191cc6d003fe3c96efbb9b6df10027bb95b2'
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'post_images#index'
+  devise_for :users
+  root to: 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  
+end
